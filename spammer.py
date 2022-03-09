9b2f41da1cc46692d872e1112356fd448ca4e3c3
eylon707 <96633129+eylon707@users.noreply.github.com>
2022-03-09 14:09:35 +0200

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,6 @@
+import pyautogui
+import keyboard 
+while True:
+    if keyboard.is_pressed('q'):
+        keyboard.write('https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=1s%27')
+        pyautogui.press("enter")
